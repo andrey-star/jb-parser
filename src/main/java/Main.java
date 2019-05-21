4169141b0d1ed62f210fbfbc7bd29fe72ff5928b
Andrey <fastrazor2000@gmail.com>
2019-05-22 00:24:46 +0300

    Catch exceptions in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,19 +6,26 @@ import main.java.exceptions.ParserException;
 import main.java.parser.Parser;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Main {
 	
-	public static void main(String[] args) throws ParserException, EvaluatingException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		List<String> lines = in.lines().collect(Collectors.toList());
-		Parser parser = new Parser();
-		Program program = parser.parseProgram(lines);
-		int res = program.run();
-		System.out.println(res);
+	public static void main(String[] args) {
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+			List<String> lines = in.lines().collect(Collectors.toList());
+			Parser parser = new Parser();
+			Program program = parser.parseProgram(lines);
+			int res = program.run();
+			System.out.println(res);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserException | EvaluatingException e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 	
 }
