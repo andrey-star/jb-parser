e2ade81469805443705a581df539dd1650c582ce
Andrey <fastrazor2000@gmail.com>
2019-05-21 22:08:49 +0300

    Change input to stdin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,14 @@ import main.java.exceptions.EvaluatingException;
 import main.java.exceptions.ParserException;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Main {
 	
-	public static void main(String[] args) throws ParserException, EvaluatingException, IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+	public static void main(String[] args) throws ParserException, EvaluatingException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		List<String> lines = in.lines().collect(Collectors.toList());
 		Program program = new Program(lines);
 		int res = program.run();
