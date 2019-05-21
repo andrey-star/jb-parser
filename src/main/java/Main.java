3dac08929f343d43b31d0c74a81280d0e77d20c8
Andrey <fastrazor2000@gmail.com>
2019-05-21 22:00:32 +0300

    Minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,23 @@
 package main.java;
 
-import main.java.elements.*;
+import main.java.elements.Program;
 import main.java.exceptions.EvaluatingException;
 import main.java.exceptions.ParserException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Main {
 	
 	public static void main(String[] args) throws ParserException, EvaluatingException, IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
 		List<String> lines = in.lines().collect(Collectors.toList());
 		Program program = new Program(lines);
 		int res = program.run();
-		System.out.println(lines);
 		System.out.println(res);
 	}
 	
